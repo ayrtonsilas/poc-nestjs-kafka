354ebd703ed2df9b801b319ee0fbc4429f2c7105
Ayrton Silas Silva Guimaraes <ayrton.guimaraes@olist.com>
2021-10-11 14:33:46 -0300

    first

--- dist/configs/configEnv.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const configEnv: {
-    environment: string;
-    port: string | number;
-    topicKafka: string;
-};
--- dist/modules/providers/kafka/kafka.service.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class KafkaService {
-    getHello(): string;
-}
